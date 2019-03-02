76fe8c8114ba3d8df8456a34ba602a4f5ba9290e
mojtaba <m_moein2@yahoo.com>
2019-03-02 10:05:14 -0800

    make commision web service

---
+++ WebApplication1/Controllers/api/Marketer/MarketerCommisionController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebApplication1.Filter;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers.api.Marketer
+{
+    public class MarketerCommisionController : ApiController
+    {
+        DBContext db = new DBContext();
+        [MarketerAuthorize]
+        [HttpPost]
+        [Route("api/MarketerCommision/GetCommisions")]
+        public object index()
+        {
+            var msg = new MarketerChat();
+            var token = System.Web.HttpContext.Current.Request.Form["Api_Token"];
+            var user = db.MarketerUsers.Where(p => p.Api_Token == token).FirstOrDefault();
+
+            var data = db.Commission.Include("MarketerFactor").Where(p => p.MarketerUser.Id == user.Id).Select(p=>new { p.Id,p.Date,p.Detail,p.Formula,p.Money}).OrderByDescending(p=>p.Id);
+
+            var paged = new PagedItem<object>(data, "");
+            return paged;
+        }
+    }
+}
